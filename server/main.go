136b802f95dad4a6ee1b76fbfbe83b8e5ab46f70
Sandy <18382255942@163.com>
2018-12-18 11:33:51 +0800

    feat(main): add http server

--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	// http请求处理
 	http.HandleFunc("/gitpull", handler1)
+	http.HandleFunc("/gitdev", handler2)
 	// 	绑定监听地址和端口
 	http.ListenAndServe(":8080", nil)
 }
@@ -16,8 +17,20 @@ func main() {
 // 请求处理函数
 func handler1(w http.ResponseWriter, r *http.Request) {
 	// 下面是执行服务器上指定的shell脚本
-	command := `./gitpull.sh`
-	cmd := exec.Command("/bin/bash", "-c", command)
+	command1 := `./gitpull.sh`
+	cmd := exec.Command("/bin/bash", "-c", command1)
+	output, err := cmd.Output()
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+	io.WriteString(w, string(output))
+}
+
+// 请求处理函数2
+func handler2(w http.ResponseWriter, r *http.Request) {
+	command2 := `./gitdev.sh`
+	cmd := exec.Command("/bin/bash", "-c", command2)
 	output, err := cmd.Output()
 	if err != nil {
 		io.WriteString(w, err.Error())
